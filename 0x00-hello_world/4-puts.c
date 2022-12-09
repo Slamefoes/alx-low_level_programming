e79e5be7062217880ad79013650689a0f4bd2c27
oluwaseun adenike oluyemi <oluyemie92@gmail.com>
2022-12-09 15:12:28 -0800

    Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Print a line of code using puts
  *
@@ -6,5 +6,7 @@
  */
 
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return (0);}
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
