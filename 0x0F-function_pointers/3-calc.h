32f29db0043e666a22c860c1def547ff48c984d0
oluwaseun adenike oluyemi <oluyemie92@gmail.com>
2023-01-20 02:09:58 -0800

    0x0F-function_pointers

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,9 +1,8 @@
 #ifndef _CALC_H_
 #define _CALC_H_
 
-#include <stdio.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * struct op - Struct op
  *
@@ -22,6 +21,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-
 
 #endif
